e99cdb38c0e035b77d76882200020ba3ef802c8f
Menchu MV <mcmvillalobos@gmail.com>
2021-10-18 21:45:19 +0200

    All data inserted except for image

--- src/componentes/card/Card.js
+++ src/componentes/card/Card.js
@@ -8,22 +8,24 @@ function Card({ gentleman }) {
         <img
           className="gentleman__avatar"
           src={fary}
-          alt="The Fary pointing at you"
+          alt={gentleman.alternativeText}
         />
         <span className="gentleman__initial">F</span>
       </div>
       <div className="gentleman__data-container">
-        <h2 className="gentleman__name">The Fary</h2>
+        <h2 className="gentleman__name">{gentleman.name}</h2>
         <ul className="gentleman__data-list">
           <li className="gentleman__data">
             <span className="gentleman__data-label">Profession:</span>
-            Influencer
+            {gentleman.profession}
           </li>
           <li className="gentleman__data">
-            <span className="gentleman__data-label">Status:</span> RIP
+            <span className="gentleman__data-label">Status:</span>{" "}
+            {gentleman.status}
           </li>
           <li className="gentleman__data">
-            <span className="gentleman__data-label">Twitter:</span> Pending
+            <span className="gentleman__data-label">Twitter:</span>{" "}
+            {gentleman.twitter}
           </li>
         </ul>
       </div>
